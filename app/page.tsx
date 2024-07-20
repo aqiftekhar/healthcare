8c355b16406568b85357bf4323663905b928d7e7
Aqdas Iftekhar <aqdasiftekhar@gmail.com>
2024-07-20 23:07:39 +0500

    Setup Project - TailwindCSS, Shadcn

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+
+    
+   <div className="text-white text-3xl underline">Home</div>
+   <Button>Click Me</Button>
+   </div>
+  );
+}
