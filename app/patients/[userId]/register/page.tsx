1edbb45e9b545f79d81d83808caf2e10c4059779
Aqdas Iftekhar <aqdasiftekhar@gmail.com>
2024-07-24 09:11:58 +0500

    Fixed UI of Registration Form

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -4,40 +4,34 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Register = async ({params: {userId}}: SearchParamProps) => {
+const Register = async ({ params: { userId } }: SearchParamProps) => {
     const user = await getUser(userId)
-  return (
-    <div className="flex h-screen max-h-screen">
+    return (
+        <div className="flex h-screen max-h-screen">
 
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
-          <Image src='/assets/icons/logo-full.svg'
-            height={1000}
-            width={1000}
-            alt="patient"
-            className="mb-12 h-10 w-fit"
-          />
+            <section className="remove-scrollbar container">
+                <div className=" max-w-[860px] flex-1 flex-col py-10">
+                    <Image src='/assets/icons/logo-full.svg'
+                        height={1000}
+                        width={1000}
+                        alt="patient"
+                        className="mb-12 h-10 w-fit"
+                    />
+                </div>
+                <RegisterForm user={user} />
+                <p className="copyright py-12">
+                    © 2024 - Connex Tech
+                </p>
+            </section>
+            <Image
+                src='/assets/images/register-img.png'
+                height={1000}
+                width={1000}
+                alt="patient"
+                className="side-img max-w-[390px]"
+            />
         </div>
-        <RegisterForm user={user}/>
-
-        <div className="text-14-regular mt-20 flex justify-between">
-          <p className="justify-items-end text-dark-600 xl:text-left">
-            © 2024 - Connex Tech
-          </p>
-          <Link href='/admin=true' className="text-green-500">
-          Admin
-          </Link>
-        </div>
-      </section>
-      <Image 
-      src='/assets/images/register-img.png'
-      height={1000}
-      width={1000}
-      alt="patient"
-      className="side-img max-w-[390px]"
-      />
-    </div>
-)
+    )
 };
 
 export default Register;
