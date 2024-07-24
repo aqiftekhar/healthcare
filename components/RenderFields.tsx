7170108695c94143367b7f561610eea5e2555249
Aqdas Iftekhar <aqdasiftekhar@gmail.com>
2024-07-24 05:35:01 +0500

    Created DateTime Picker Component, Radio Group, Skeleton and a Form for Personal Information

--- components/RenderFields.tsx
+++ components/RenderFields.tsx
@@ -6,8 +6,12 @@ import { FormControl } from "./ui/form";
 import 'react-phone-number-input/style.css';
 import PhoneInput from 'react-phone-number-input'
 import { E164Number } from "libphonenumber-js/core";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
 export const RenderFields = ({ field, props }: { field: any, props: ICustomProps }) => {
-    const { fieldType, iconSrc, iconAlt, placeholder } = props;
+    const { fieldType, iconSrc, iconAlt, placeholder, showTimeSelect, dateFormat, renderSkeleton } = props;
     switch (fieldType) {
         case FormFieldTypes.INPUT:
             return (
@@ -22,27 +26,54 @@ export const RenderFields = ({ field, props }: { field: any, props: ICustomProps
                         />
                     )}
                     <FormControl>
-                        <Input 
-                            placeholder={placeholder} 
-                            {...field} 
+                        <Input
+                            placeholder={placeholder}
+                            {...field}
                             className="shad-input border-0"
-                            />
+                        />
                     </FormControl>
                 </div>
             )
         case FormFieldTypes.PHONE_INPUT:
             return (
                 <FormControl>
-                    <PhoneInput 
-                    defaultCountry="PK"
-                    placeholder={placeholder}
-                    international
-                    withCountryCallingCode
-                    value={field.value as E164Number | undefined}
-                    onChange={field.onChange}
-                    className="input-phone"
+                    <PhoneInput
+                        defaultCountry="PK"
+                        placeholder={placeholder}
+                        international
+                        withCountryCallingCode
+                        value={field.value as E164Number | undefined}
+                        onChange={field.onChange}
+                        className="input-phone"
                     />
                 </FormControl>
+            )
+        case FormFieldTypes.DATE_PICKER:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    <Image
+                        src="/assets/icons/calendar.svg"
+                        height={24}
+                        width={24}
+                        alt="calendar"
+                        className="ml-2"
+                    />
+                    <FormControl>
+                        <DatePicker
+                            selected={field.value}
+                            onChange={(date) => field.onChange(date)}
+                            dateFormat={dateFormat ?? 'MM/dd/yyyy'}
+                            showTimeSelect={showTimeSelect ?? false}
+                            timeInputLabel="Time:"
+                            wrapperClassName="date-picker"
+                        />
+                    </FormControl>
+
+                </div>
+            )
+        case FormFieldTypes.SKELETON:
+            return (
+                renderSkeleton ? renderSkeleton(field) : null
             )
         default:
             break;
--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Form } from "@/components/ui/form"
+import { Form, FormControl } from "@/components/ui/form"
 import CustomFormFields from "../CustomFormFields"
 import { FormFieldTypes } from "@/lib/FormFieldTypes"
 import SubmitButton from "../SubmitButton"
@@ -11,14 +11,16 @@ import { useState } from "react"
 import { UserFormValidation } from "@/lib/FormValidation"
 import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.actions"
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import { GenderOptions } from "@/constants"
 
 
 
 
-const RegisterForm = ({user} : {user: User}) => {
+const RegisterForm = ({ user }: { user: User }) => {
     const router = useRouter();
     const [isLoading, setisLoading] = useState(false);
-    
+
     // 1. Define your form.
     const form = useForm<z.infer<typeof UserFormValidation>>({
         resolver: zodResolver(UserFormValidation),
@@ -33,19 +35,19 @@ const RegisterForm = ({user} : {user: User}) => {
     async function onSubmit(values: z.infer<typeof UserFormValidation>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
-        
+
         setisLoading(true);
-        
+
         try {
-            
+
             const user_data = {
                 name: values.name,
                 email: values.email,
                 phone: values.phone,
-              };
-            
+            };
+
             const user = await createUser(user_data);
-              
+
             if (user) router.push(`/patients/${user.$id}/register`)
         } catch (error) {
             console.log(error)
@@ -55,10 +57,16 @@ const RegisterForm = ({user} : {user: User}) => {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-                <section className="mb-12 space-y-4">
-                    <h1 className="header"> Hi There 👋 </h1>
-                    <p className="text-dark-700">Schedule your appointments.</p>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
+                <section className="space-y-4">
+                    <h1 className="header"> Welcome 👋 </h1>
+                    <p className="text-dark-700">Let us know more about yourself.</p>
+                </section>
+                <section className="space-y-6">
+                    <div className="mb-9 space-y-1">
+                        <h2 className="sub-header">Personal Information.</h2>
+                    </div>
+
                 </section>
                 <CustomFormFields
                     fieldType={FormFieldTypes.INPUT}
@@ -69,7 +77,70 @@ const RegisterForm = ({user} : {user: User}) => {
                     iconAlt="user"
                     control={form.control}
                 />
-                
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormFields
+                        fieldType={FormFieldTypes.INPUT}
+                        name="email"
+                        label="Email"
+                        placeholder="john@email.com"
+                        iconSrc="/assets/icons/email.svg"
+                        iconAlt="email"
+                        control={form.control}
+                    />
+                    <CustomFormFields
+                        fieldType={FormFieldTypes.PHONE_INPUT}
+                        name="phone"
+                        label="Phone Number"
+                        placeholder="(555) 123-4567"
+                        control={form.control}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormFields
+                        fieldType={FormFieldTypes.DATE_PICKER}
+                        name="birthDate"
+                        label="Date Of Birth"
+                        placeholder="john@email.com"
+                        iconSrc="/assets/icons/email.svg"
+                        iconAlt="email"
+                        control={form.control}
+                    />
+                    <CustomFormFields
+                        fieldType={FormFieldTypes.SKELETON}
+                        name="gender"
+                        label="Gender"
+                        control={form.control}
+                        renderSkeleton={(field) => (
+                            <FormControl>
+                                <RadioGroup className="flex h-11 gap-6 xl:justify-between" 
+                                onValueChange={field.onChange} 
+                                defaultValue={field.value}>
+                                    {GenderOptions.map((option) => (
+                                        <div key={option} className="radio-group">
+                                            <RadioGroupItem value={option} id={option} />
+                                            <label htmlFor={option} className="cursor-pointer">
+                                                {option}
+                                            </label>
+                                        </div>
+                                    ))}
+                                </RadioGroup>
+
+                            </FormControl>
+                        )}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-6 xl:flex-row">
+
+                </div>
+                <div className="flex flex-col gap-6 xl:flex-row">
+
+                </div>
+
+                <div className="flex flex-col gap-6 xl:flex-row">
+
+                </div>
                 <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
             </form>
         </Form>
