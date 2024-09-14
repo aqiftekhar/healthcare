695dcf1a35a979d25a8962a89ed758b9c78cda4a
Aqdas Iftekhar <aqdasiftekhar@gmail.com>
2024-09-14 22:57:37 +0500

    changed name

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -104,7 +104,7 @@ export const updateAppointment = async ({
 
     if (!updatedAppointment) throw Error('Appointment not Found');
 
-    const smsMessage = `Greetings from AI MedPulse. ${type === "schedule" ? `Your appointment is confirmed for ${formatDateTime(appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}` : `We regret to inform that your appointment for ${formatDateTime(appointment.schedule!).dateTime} is cancelled. Reason:  ${appointment.cancellationReason}`}.`;
+    const smsMessage = `Greetings from CarePulse. ${type === "schedule" ? `Your appointment is confirmed for ${formatDateTime(appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}` : `We regret to inform that your appointment for ${formatDateTime(appointment.schedule!).dateTime} is cancelled. Reason:  ${appointment.cancellationReason}`}.`;
     await sendSMSNotification(userId, smsMessage);
 
      revalidatePath("/admin");
